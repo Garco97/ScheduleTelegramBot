6d96fe53bc93c7efebd73198493dc457e1828fbf
Diego <diegometalgarcia@gmail.com>
2018-09-17 23:38:22 +0200

    Prueba 2

--- Scheduler.py
+++ Scheduler.py
@@ -3,7 +3,7 @@ from telegram.ext import Updater, CommandHandler
 
 def hello(bot, update):
     update.message.reply_text(
-        'Hello {}'.format(update.message.from_user.first_name))
+        'Hola {}'.format(update.message.from_user.first_name))
 
 
 updater = Updater('697984917:AAEPmyM3LemXq5MkEcepsrzb927Im9_wsAA')
