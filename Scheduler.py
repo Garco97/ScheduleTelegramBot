7d68c0783ff2ff9b2c6910bbd6cc4ab25fc993dc
Diego <diegometalgarcia@gmail.com>
2018-09-18 11:22:54 +0200

    First files, schedule not complete

--- Scheduler.py
+++ Scheduler.py
@@ -1,16 +1,41 @@
 from telegram.ext import Updater, CommandHandler
+import datetime
 
 
-def hello(bot, update):
-    update.message.reply_text(
-        'Adios{}'.format(update.message.from_user.first_name))
+def horario(bot,update):
 
+	file = open("C:/Users/Diego/SchedulerBot/Horario.txt")
+	day = datetime.datetime.now()
+	for line in file:	
+		if day.strftime("%A") in line:
+			update.message.reply_text(line )
+			for line2 in file:
+				if line2  in ["Monday", "Tuesday", "Wednesday","Thursday" ,"Friday"]:
+					print("No more classes")
+					break
+				horaInicio, separador,finAsignatura = line2.partition("-")
+				horaFinal,separador,asignatura = finAsignatura.partition("_")
+				hora, separador, minuto = horaFinal.partition(":")			
+				final = (int(hora)*60) + int(minuto)
+				now = datetime.datetime.now()			
+				actual = (now.hour*60) + now.minute		
+				if actual < final:
+					
 
-updater = Updater('697984917:AAEPmyM3LemXq5MkEcepsrzb927Im9_wsAA')
+					update.message.reply_text(horaInicio + "-" + horaFinal + " " + asignatura)
 
-updater.dispatcher.add_handler(CommandHandler('ello', hello))
+					
+			pass
+	pass
 
-updater.start_polling()
-updater.idle
+	
+def main():
+	updater = Updater('697984917:AAEPmyM3LemXq5MkEcepsrzb927Im9_wsAA')
 
+	updater.dispatcher.add_handler(CommandHandler('horario', horario))
+	updater.start_polling()
+	updater.idle
 
+	print("Ejecutando")
+if __name__ == '__main__':
+    main()
