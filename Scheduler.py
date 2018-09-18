b4a2df38367ac7d28899155f8d7484be5298c232
Diego <diegometalgarcia@gmail.com>
2018-09-19 00:41:25 +0200

    Link to Schedule added

--- Scheduler.py
+++ Scheduler.py
@@ -7,7 +7,7 @@ def horario(bot,update):
 
 	file = urllib.request.urlopen("https://raw.githubusercontent.com/Garco97/ScheduleTelegramBot/master/Horario.txt")	
 	day = datetime.datetime.now()
-	print (now.hour,":",now.minute) 
+	print (now.hour + ":" + now.minute) 
 	for line in file:
 		line = line.decode('utf-8')
 		if day.strftime("%A") in line:
