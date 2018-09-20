305f014553b253f99b89f08d68d9d121fb89ea2d
Diego García Cosío <diego@MacBook-Air-de-Diego.local>
2018-09-20 19:09:55 +0200

    Arreglos

--- Scheduler.py
+++ Scheduler.py
@@ -3,29 +3,28 @@ import datetime
 import urllib.request
 
 def horario(bot,update):
-		
 
-	file = urllib.request.urlopen("https://raw.githubusercontent.com/Garco97/ScheduleTelegramBot/master/Horario.txt")	
+
+	file = urllib.request.urlopen("https://raw.githubusercontent.com/Garco97/ScheduleTelegramBot/master/Horario.txt")
 	day = datetime.datetime.now()
-	 
+
 	for line in file:
 		line = line.decode('utf-8')
 		if day.strftime("%A") in line:
 			update.message.reply_text(line )
 			for line2 in file:
-				line2 = line2.decode('utf-8')
-				print(line2)	
+				line2 = line2.decode('utf-8')	
 				if line2  in ["Monday", "Tuesday", "Wednesday","Thursday" ,"Friday"]:
 					print("No more")
 					break
-				
+
 				horaInicio, separador,finAsignatura = line2.partition("-")
 				horaFinal,separador,asignatura = finAsignatura.partition("_")
-				hora, separador, minuto = horaFinal.partition(":")			
+				hora, separador, minuto = horaFinal.partition(":")
 				final = (int(hora)*60) + int(minuto)
-				now = datetime.datetime.now()			
-				actual = ((now.hour+2)*60) + now.minute		
-				
+				now = datetime.datetime.now()
+				actual = ((now.hour+2)*60) + now.minute
+
 				if actual < final:
 					update.message.reply_text(horaInicio + "-" + horaFinal + " " + asignatura)
 			pass
