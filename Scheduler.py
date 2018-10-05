b1649bd50d9fbfbce99c184ed39db69626a23739
Diego García Cosío <diego@MacBook-Air-de-Diego.local>
2018-10-05 23:38:54 +0200

    Añadido el comando de los trabajos y practicas

--- Scheduler.py
+++ Scheduler.py
@@ -4,39 +4,32 @@ import urllib.request
 
 def hoy(bot,update):
 	user = update.message.from_user
-	if(user.id == 223472488):
-		file = urllib.request.urlopen("https://raw.githubusercontent.com/Garco97/ScheduleTelegramBot/master/Horario.txt")
-		day = datetime.datetime.now()
-		for line in file:
-			line = line.decode('utf-8')
-			if day.strftime("%A") in line:
-				update.message.reply_text(line)
-				for line2 in file:
-					line2 = line2.decode('utf-8')
-					if line2  in ["Monday", "Tuesday", "Wednesday","Thursday" ,"Friday"]:
-						print("No more")
-						break
-					horaInicio, separador,finAsignatura = line2.partition("-")
-					horaFinal,separador,asignatura = finAsignatura.partition("_")
-					hora, separador, minuto = horaFinal.partition(":")
-					final = (int(hora)*60) + int(minuto)
-					now = datetime.datetime.now()
-					actual = ((now.hour+2)*60) + now.minute
-
-					if actual < final:
-						update.message.reply_text(horaInicio + "-" + horaFinal + " " + asignatura)
-	else:
-		update.message.reply_text("No tienes horario asignado")
+	file = urllib.request.urlopen("https://raw.githubusercontent.com/Garco97/ScheduleTelegramBot/master/Horario.txt")
+	day = datetime.datetime.now()
+	for line in file:
+		line = line.decode('utf-8')
+		if day.strftime("%A") in line:
+			update.message.reply_text(line)
+			for line2 in file:
+				line2 = line2.decode('utf-8')
+				if line2  in ["Monday", "Tuesday", "Wednesday","Thursday" ,"Friday"]:
+					print("No more")
+					break
+				horaInicio, separador,finAsignatura = line2.partition("-")
+				horaFinal,separador,asignatura = finAsignatura.partition("_")
+				hora, separador, minuto = horaFinal.partition(":")
+				final = (int(hora)*60) + int(minuto)
+				now = datetime.datetime.now()
+				actual = ((now.hour+2)*60) + now.minute
+				if actual < final:
+					update.message.reply_text(horaInicio + "-" + horaFinal + " " + asignatura)
 
 def horario(bot,update):
 	update.message.reply_text("https://web.unican.es/centros/ciencias/Documents/Horarios%201819/Grado/G-Inf%202018-19%20V6.pdf")
 
 
 def trabajosPendientes(bot,update):
-	user = update.message.from_user
-	if(user.id == 223472488):
-		file = open("../ScheduleTelegramBot/trabajos.txt")
-		file = urllib.request.urlopen("https://raw.githubusercontent.com/Garco97/ScheduleTelegramBot/master/trabajos.txt")
+	file = urllib.request.urlopen("https://raw.githubusercontent.com/Garco97/ScheduleTelegramBot/master/trabajos.txt")
 		for line in file:
 			line = line.decode('utf-8')
 			trabajo,separador,resto = line.partition("_")
